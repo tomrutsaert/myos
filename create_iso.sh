3e78059da170d317c4b296bf104cf344e0a2e1ab
Tom Rutsaert <tom.rutsaert@inmail.be>
2024-12-08 21:27:09 +0100

    remove razer software

--- create_iso.sh
+++ create_iso.sh
@@ -4,8 +4,7 @@ mkdir -p ./iso-output
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
 ghcr.io/jasonn3/build-container-installer:latest \
 IMAGE_REPO=ghcr.io/tomrutsaert \
-IMAGE_NAME=myos-hyprland-main \
-IMAGE_TAG=latest \
+IMAGE_NAME=myos-gnome-main \
+IMAGE_TAG=latest 
 VERSION=41 \
-VARIANT=Server
-#VARIANT=Silverblue
+VARIANT=Silverblue
