d1f8f0a13548d5632c28c39caa84d093f0d696a5
Tom Rutsaert <tom.rutsaert@inmail.be>
2024-12-08 15:23:42 +0100

    bluebuild action update + flatpaks to just

--- create_iso.sh
+++ create_iso.sh
@@ -4,7 +4,8 @@ mkdir -p ./iso-output
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
 ghcr.io/jasonn3/build-container-installer:latest \
 IMAGE_REPO=ghcr.io/tomrutsaert \
-IMAGE_NAME=myos-gnome-main \
+IMAGE_NAME=myos-hyprland-main \
 IMAGE_TAG=latest \
-VERSION=40 \
-VARIANT=Silverblue
+VERSION=41 \
+VARIANT=Server
+#VARIANT=Silverblue
