5f4a0323de7d0939f222f4f337af1cbab8c427a4
Tom Rutsaert <tom.rutsaert@inmail.be>
2025-11-20 23:49:23 +0100

    leaner sway

--- create_iso.sh
+++ create_iso.sh
@@ -4,7 +4,7 @@ mkdir -p ./iso-output
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
 ghcr.io/jasonn3/build-container-installer:latest \
 IMAGE_REPO=ghcr.io/tomrutsaert \
-IMAGE_NAME=myos-gnome-main \
+IMAGE_NAME=myos-sway-main \
 IMAGE_TAG=latest 
-VERSION=41 \
+VERSION=43 \
 VARIANT=Silverblue
